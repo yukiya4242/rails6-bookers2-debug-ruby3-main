b78d64d7dab24c622676c22f210c5bc1878d3d40
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-19 23:42:10 +0000

    start

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   devise_for :users
-  
-  get "home/about"=>"homes#about"
+
+  get "homes/about"=>"homes#about"
   root :to =>"homes#top"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update]
   resources :users, only: [:index,:show,:edit,:update]

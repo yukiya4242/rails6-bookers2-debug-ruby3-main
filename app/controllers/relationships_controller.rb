ee647873828fe1fb177f5ae29a43b00db1713cb3
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-22 09:22:38 +0000

    unfollowできた

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -9,8 +9,14 @@ class RelationshipsController < ApplicationController
 
 
   def destroy
-    user = Relationship.find(params[:id]).followed
+    user = User.find(params[:followed_id])
     current_user.unfollow(user)
     redirect_to user
   end
+
+  # def destroy
+  #   user = Relationship.find(params[:id]).followed
+  #   current_user.unfollow(user)
+  #   redirect_to user
+  # end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+//= require anime.min
+
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
@@ -11,8 +13,9 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+

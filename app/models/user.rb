b7f8d7222d154e1b9b4ae141a92c843334862194
yukiya4242 <126574597+yukiya4242@users.noreply.github.com>
2023-03-22 00:41:49 +0000

    none

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,28 @@ class User < ApplicationRecord
   # dependent: :destroyは、has_manyで使えるオプションです。 1:Nの関係において、
   # 「1」のデータが削除された場合、関連する「N」のデータも削除される設定です。
   # この設定では、Userのデータが削除されたとき、そのUserが投稿したコメントデータも一緒に削除されます。
+
+  # いいね、コメント--------------------------------------------
    has_many :book_comments, dependent: :destroy
    has_many :favorites, dependent: :destroy
 
 
-   def like(book)
+  # フォローフォロワー-------------------------------------
+  has_many :active_relationshiops, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+def following
+  active_relationshiops.map { |r| r.followed }
+end
+
+def followers
+  passive_relationships.map { |r| r.follower }
+end
+
+
+
+
+def like(book)
   favorites.create(book_id: book.id)
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_21_095631) do
+ActiveRecord::Schema.define(version: 2023_03_22_000101) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 2023_03_21_095631) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["book_id"], name: "index_favorites_on_book_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "folloewd_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
